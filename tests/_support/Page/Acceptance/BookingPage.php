bd07fa0a4216339b39be85f027f81823b1b69e4c
thanhbinh712 <trinhthanhbinh.ttb@gmail.com>
2019-05-10 11:17:03 +0700

    first commit

---
+++ tests/_support/Page/Acceptance/BookingPage.php
@@ -0,0 +1,58 @@
+<?php
+namespace Page\Acceptance;
+
+class BookingPage
+{
+    // include url of current page
+    /**
+     * @var string
+     */
+    public static $URL = '/reservationform';
+    /**
+     * @var string
+     */
+    public static $tableID = '//input[@name="tableID"]';
+    /**
+     * @var string
+     */
+    public static $numOfPerson = '//input[@name="numberofperson"]';
+    /**
+     * @var string
+     */
+    public static $date = '//input[@name="date"]';
+    /**
+     * @var string
+     */
+    public static $description = '//textarea[@name="description"]';
+    /**
+     * @var string
+     */
+    public static $startTime = '//input[@name="startTime"]';
+
+    /**
+     * Declare UI map for this page here. CSS or XPath allowed.
+     * public static $usernameField = '#username';
+     * public static $formSubmitButton = "#mainForm input[type=submit]";
+     */
+
+    /**
+     * Basic route example for your current URL
+     * You can append any additional parameter to URL
+     * and use it in tests like: Page\Edit::route('/123-post');
+     */
+    public static function route($param)
+    {
+        return static::$URL.$param;
+    }
+
+    /**
+     * @var \AcceptanceTester;
+     */
+    protected $acceptanceTester;
+
+    public function __construct(\AcceptanceTester $I)
+    {
+        $this->acceptanceTester = $I;
+    }
+
+}
---
+++ tests/_support/Page/Acceptance/StartPage.php
@@ -0,0 +1,52 @@
+<?php
+namespace Page\Acceptance;
+
+class StartPage
+{
+    // include url of current page
+    public static $URL = '/QATest';
+    /**
+     * @var string
+     */
+    public static $newReservation = '//a[@href="index.newreservation"]';
+    /**
+     * @var string
+     */
+    public static $reloadData = '//a[@href="index.reload"]';
+    /**
+     * @var string
+     */
+    public static $goToPage1 = '//a[@title="Go to page 1"]';
+    /**
+     * @var string
+     */
+    public static $goToPage2 = '//a[@title="Go to page 2"]';
+
+
+    /**
+     * Declare UI map for this page here. CSS or XPath allowed.
+     * public static $usernameField = '#username';
+     * public static $formSubmitButton = "#mainForm input[type=submit]";
+     */
+
+    /**
+     * Basic route example for your current URL
+     * You can append any additional parameter to URL
+     * and use it in tests like: Page\Edit::route('/123-post');
+     */
+    public static function route($param)
+    {
+        return static::$URL.$param;
+    }
+
+    /**
+     * @var \AcceptanceTester;
+     */
+    protected $acceptanceTester;
+
+    public function __construct(\AcceptanceTester $I)
+    {
+        $this->acceptanceTester = $I;
+    }
+
+}
